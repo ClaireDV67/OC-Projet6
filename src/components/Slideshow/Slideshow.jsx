c012ad9a3e5795fb51ed602b75f641f2f720f2f0
Claire De Vito <claire.devito@gmail.com>
2023-04-15 11:34:45 +0200

    Ajout du compteur Slideshow

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -32,11 +32,13 @@ function Slideshow( {pictures, title} ) {
     return pictures.length === 1 ? ( 
             <div className={styles.carousel}>
                 <img className={styles.slide} src={pictures[count]} alt={title} title={title} />
+                <span className={styles.counter}>{count+1}/{pictures.length}</span>
             </div>
         ) : (
         <div className={styles.carousel}>
             <button aria-label="Image précédente" className={styles.button + " " + styles.arrowLeft} onClick={() => previousSlide()}><i className={styles.arrow + " fa-solid fa-chevron-left"}></i></button>
             <img className={styles.slide} animation={animation} src={pictures[count]} alt={title} title={title}/>
+            <span className={styles.counter}>{count+1}/{pictures.length}</span>
             <button aria-label="Image suivante" className={styles.button +  " " + styles.arrowRight} onClick={() => nextSlide()}><i className={styles.arrow + " fa-solid fa-chevron-right"}></i></button>
         </div>
         )
